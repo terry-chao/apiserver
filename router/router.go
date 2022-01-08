65a1d1bb3039617cbf44036ac18abe2a6483787a
terrych322 <acchn@live.com>
2022-01-08 17:28:08 +0800

    feat: 自定义业务错误信息

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"apiserver/handler/user"
 	"net/http"
 
 	"apiserver/handler/sd"
@@ -21,6 +22,11 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
+
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
+	}
 
 	// The health check handlers
 	svcd := g.Group("/sd")
