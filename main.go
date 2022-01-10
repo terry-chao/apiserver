7cb2e20124cb4066370c57796af956819e8fd8b2
terrych322 <acchn@live.com>
2022-01-10 22:43:36 +0800

    feat: 给api命令添加版本功能

--- main.go
+++ main.go
@@ -3,24 +3,41 @@ package main
 import (
 	"apiserver/config"
 	"apiserver/model"
+	v "apiserver/pkg/version"
 	"apiserver/router"
 	"apiserver/router/middleware"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	mylog "github.com/zxmrlc/log"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 var (
-	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+	cfg     = pflag.StringP("config", "c", "", "apiserver config file path.")
+	version = pflag.BoolP("version", "v", false, "show version info.")
 )
 
 func main() {
 	pflag.Parse()
+
+	if *version {
+		v := v.Get()
+		marshalled, err := json.MarshalIndent(&v, "", "  ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(marshalled))
+		return
+	}
 
 	// init config
 	if err := config.Init(*cfg); err != nil {
